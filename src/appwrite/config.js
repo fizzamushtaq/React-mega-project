866d4f1c5378ea4b74f531953e420a58429f07b3
Fizza <fizach581@gmail.com>
2024-09-16 15:28:18 +0500

    mega project

---
+++ src/appwrite/config.js
@@ -0,0 +1,124 @@
+import conf from "../conf/conf";
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service {
+  client = new Client();
+  database;
+  storage;
+
+  constructor() {
+    this.client.setEndpoint(conf.appwriteUrl).setProject(conf.appwriteProject);
+    this.database = new Databases(this.client);
+    this.storage = new storage(this.client);
+  }
+
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      return await this.database.createDocument(
+        conf.appwriteDataBaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        }
+      );
+    } catch (error) {
+      console.log("Appwrite serive :: uploadPost :: error", error);
+    }
+  }
+
+  async uploadPost(slug, { title, content, featuredImage, status }) {
+    try {
+      return await this.database.updateDocument(
+        conf.appwriteDataBaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+        }
+      );
+    } catch (error) {
+      console.log("Appwrite serive :: uploadPost :: error", error);
+    }
+  }
+
+  async deletePost(slug) {
+    try {
+      await this.database.deleteDocument(
+        conf.appwriteDataBaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+      return true;
+    } catch (error) {
+      console.log("Appwrite serive :: deletePost :: error", error);
+      return false;
+    }
+  }
+  // this method use to get one post
+  async getPost(slug) {
+    try {
+      return await this.database.getDocument(
+        conf.appwriteDataBaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+    } catch (error) {
+      console.log("Appwrite serive :: getPost :: error", error);
+      return false;
+    }
+  }
+  // this method use to get  post that have active status
+
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.database.listDocuments(
+        conf.appwriteDataBaseId,
+        conf.appwriteCollectionId,
+        queries
+      );
+    } catch (error) {
+      console.log("Appwrite serive :: getPosts :: error", error);
+      return false;
+    }
+  }
+  
+  // file upload method
+
+  async uploadFile(file) {
+    try {
+      return await this.storage.createFile(
+        conf.appwriteBucketId,
+        ID.unique(),
+        file
+      );
+    } catch (error) {
+      console.log("Appwrite serive :: uploadFile :: error", error);
+      return false;
+    }
+  }
+
+  async deleteFile(fileId) {
+    try {
+      await this.storage.deleteFile(conf.appwriteBucketId, fileId);
+      return true;
+    } catch (error) {
+      console.log("Appwrite serive :: deleteFile :: error", error);
+      return false;
+    }
+  }
+
+  getFilePreview(fileId) {
+    return this.storage.getFilePreview(conf.appwriteBucketId, fileId);
+  }
+}
+
+const service = new Service();
+export default service;
---
+++ src/conf/conf.js
@@ -0,0 +1,9 @@
+const conf = {
+  appwriteUrl: String(import.meta.env.VITE_APP_APPWRITE_URL),
+  appwriteProject: String(import.meta.env.VITE_APP_APPWRITE_PROJECT_ID),
+  appwriteDataBaseId: String(import.meta.env.VITE_APP_APPWRITE_DATABASE_ID),
+  appwriteCollectionId: String(import.meta.env.VITE_APP_APPWRITE_COLLECTION_ID),
+  appwriteBucketId: String(import.meta.env.VITE_APP_APPWRITE_BUCKET_ID),
+};
+
+export default conf;
