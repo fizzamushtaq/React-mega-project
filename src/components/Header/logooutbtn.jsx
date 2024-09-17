1eff2511b58ee27fce871c7b3d34898a6eb0274b
Fizza <fizach581@gmail.com>
2024-09-18 02:04:59 +0500

    last commit

--- src/components/Header/logooutbtn.jsx
+++ src/components/Header/logooutbtn.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import service from '../../appwrite/config'
+import AuthService from '../../appwrite/Auth'
 import { logout } from '../../store/store'
 
 function Logooutbtn() {
-
-
+    const dispatch = useDispatch()
+    const logoutHandler = () => {
+        AuthService.logout().then(() => {
+            dispatch(logout())
+        })
+    }
     return (
         <button
         className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
