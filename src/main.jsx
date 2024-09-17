6a54b15623ddeddfe7fe466d1d0ce2ea3e3bf067
Fizza <fizach581@gmail.com>
2024-09-18 01:55:23 +0500

    update header

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import { createStore, compose, applyMiddleware } from 'redux'
+
+// const enchancers = compose
+// const store = createStore (
+//   rootReducer,
+//   enchancers(applyMiddleware(thunk))
+// );
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
