e88453b03c22096b8ce2b7901a937971c4dd9d00
Fizza <fizach581@gmail.com>
2024-09-25 03:46:57 +0500

    half of the  login

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,27 @@
 import Header from "./Header/header";
 import Footer from "./Footer/footer";
+import RTE from "./RTE";
 import Logo from "./logo";
 import container from "./container/container"
 import logooutbtn from "./Header/logooutbtn"
+import Login from "./login";
+import button from "../components/button"
+import Select from "./select";
+
 
 export {
     Header,
     Footer,
     Logo,
     container,
-    logooutbtn
+    logooutbtn,
+    RTE,
+    Input,
+    button,
+    Login,
+    Select,
+    
+
+
+    
 }
---
+++ src/components/login.jsx
@@ -0,0 +1,13 @@
+import React , {useState} from 'react'
+import {link , useNavigate} from 'react-router-dom'
+import {login as authLogin} from '../store/store' 
+import {button, Logo , Input} from '../components/index'
+
+
+function Login() {
+    return (
+        <div>text</div>
+    )
+}
+
+export default Login
