d9c27f1c52f49e3cfc2dc35f6872a59595f26b4f
Fizza <fizach581@gmail.com>
2024-11-07 23:45:22 +0500

    editpost

---
+++ src/pages/editpost.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useEffect , useState } from 'react'
+import Container from '../components/container/container'
+import { Postform } from '../components'
+import { appwriteService } from "../appwrite/config";
+// uncomplete
+function Editpost() {
+    return (
+        <div>
+            edit post
+        </div>
+    )
+}
+
+export default Editpost
