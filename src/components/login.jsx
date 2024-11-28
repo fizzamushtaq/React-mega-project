c4250ad3a68d5b7bf3d60f68219fbd37db1d632b
Fizza <fizach581@gmail.com>
2024-11-28 17:18:22 +0500

    fix signup bug

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -42,7 +42,7 @@ function Login() {
           </span>
         </div>
         <h2 className="text-center text-2xl font-bold leading-tight">
-          Sign in to your account
+          Sign in to your account for
         </h2>
         <p className="mt-2 text-center text-base text-black/60">
           Don&apos;t have any account?&nbsp;
